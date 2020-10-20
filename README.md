04e0516b4c8f17188f9d78c66ae000dabf2a9cd1
Joy Jefferson <joy.jefferson10@gmail.com>
2020-10-20 09:51:29 +0530

    added VC.py

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # VideoCapture
+  The code VideoCapture.py is used to access webcam to implement American sign language detection(No worries the issue isn't related to ASL).
+  VideoCapture.py captures every third frame, presently the when the code is executed,the feed screen is
+
+  
+  We expect you to implement a green bounding box(region where the actual sign is captured) as shown 
+  below in the picture
+# TechStack
+  * Python
+  * PILLOW
+  * opencv
+  * numpy 
+  * keras
+# How do I contribute?
+  Check out the contributing guidelines to learn more!
+  
+# Is this beginner friendly?
+  YES!
+# What if I have a problem?
+  Contact any of the ACM Team members!
+  **This is one of the official repositories created by ACM PESU-ECC for hacktoberfest 2020!** 
---
+++ VideoCapture.py
@@ -0,0 +1,65 @@
+
+
+import numpy as np
+import cv2
+import time
+
+from PIL import Image
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+
+#model = load_model("D:/Sign language detection/model4_0.9823.h5")
+
+num_frames=0
+cap=cv2.VideoCapture(0)
+width  = cap.get(3) 
+height = cap.get(4)
+
+data = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+
+top_left = (int(0.05*width), int(0.2*height))
+bottom_right = (int(0.35*width), int(0.55*height))
+
+
+
+
+prev = int(time.time())
+k=0
+
+while(True):
+
+    ret,frame=cap.read()
+
+    
+    cv2.imshow("from Video Feed",frame)
+    k=cv2.waitKey(1)
+
+    if(int(time.time()) == (prev + 2)):
+        #ret,frame=cap.read()
+        #frame=imutils.resize(frame,width=700)
+        #frame = cv2.flip(frame, 1)
+        clone=frame.copy()
+        
+
+        #ret,frame=cap.read()
+        roi=frame[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]]
+
+        roi = cv2.resize(roi, (128,128))
+        roi = cv2.normalize(roi, None, alpha=0, beta = 1, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+        roi = roi[np.newaxis,:]
+        #roi = data.apply_transform(roi)
+        #cv2.imshow('Sign shown',roi)
+        #print(np.argmax(model.predict(roi)))
+        #cv2.waitKey(125)
+        prev=int(time.time())
+
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
